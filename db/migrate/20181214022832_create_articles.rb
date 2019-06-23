7ed7eb9624c3f667b67100a41514c714e94cbca0
bjhoohaha <cvwo271828182845045>
2019-06-24 00:32:27 +0800

    added columns in database

--- db/migrate/20181214022832_create_articles.rb
+++ db/migrate/20181214022832_create_articles.rb
@@ -2,8 +2,9 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
       t.string :title
+      t.integer :year
+      t.string :category
       t.text :text
-
       t.timestamps
     end
   end
